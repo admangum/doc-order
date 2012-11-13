8ab3d8682762d5b4c5dc875dd5d7665cf63c8486
Adam Mangum <afmangum@gmail.com>
2012-11-13 14:56:41 -0800

    Moved logic into a constructor function and added per-instance access to API object to allow calling "update" manually

--- jquery.docorder.js
+++ jquery.docorder.js
@@ -1,101 +1,116 @@
 //	jquery.docorder.js 1.0
 //	2012 Adam Mangum
 
-(function($){
-	$.fn.docorder = function( options ){
-		
-		var settings = $.extend({
-			'latency': 1000,
-			'queries': [],
-		}, options),		
-		
-		//     Debounce function from:
-		//	   Underscore.js 1.4.2
-		//     http://underscorejs.org
-		//     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
-		//     Underscore may be freely distributed under the MIT license.
-		
-		debounce = function(func, wait, immediate) {
-		    var timeout, result;
-		    return function() {
-		      var context = this, args = arguments;
-		      var later = function() {
-		        timeout = null;
-		        if (!immediate) result = func.apply(context, args);
-		      };
-		      var callNow = immediate && !timeout;
-		      clearTimeout(timeout);
-		      timeout = setTimeout(later, wait);
-		      if (callNow) result = func.apply(context, args);
-		      return result;
-		    };
-		  },
-		
-		move = function( mappings ){
-			var map, sibs;
-			$.each(mappings, function(){
-				map = this;
-				map.parent 	= map.parent instanceof jQuery ? map.parent : $(map.parent);
-				map.child 	= map.child instanceof jQuery ? map.child : $(map.child);
-				sibs 		= map.parent.children();
-				if( !map.parent || !map.child )
-					return;
-				if( map.index === 0 ){
-					map.child.prependTo( map.parent );
-				}else if( typeof map.index == 'number' && map.index < sibs.length ){
-					sibs.eq(map.index).before(map.child);
-				}else{
-					map.child.appendTo( map.parent );
-				}
-			});
-		},
-		
-		/**
-		*	Cache the location and position of each
-		*	child element that will move about the DOM
-		*	so that they can be returned to their initial
-		*	state whenever none of the media queries apply
-		*/
-		
-		initState = (function(a){
+;(function($, window, document, undefined){
+
+	var 
+	name = 'docorder',
+	defaults = {
+		'autoUpdate': true,
+		'latency': 1000,
+		'queries': [],
+	},
+	/**
+	     Debounce function from:
+		 Underscore.js 1.4.2
+	     http://underscorejs.org
+	     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
+	     Underscore may be freely distributed under the MIT license.
+	
+	*/
+	_debounce = function debounce(func, wait, immediate) {
+	    var timeout, result;
+	    return function() {
+	      var context = this, args = arguments;
+	      var later = function() {
+	        timeout = null;
+	        if (!immediate) result = func.apply(context, args);
+	      };
+	      var callNow = immediate && !timeout;
+	      clearTimeout(timeout);
+	      timeout = setTimeout(later, wait);
+	      if (callNow) result = func.apply(context, args);
+	      return result;
+	    };
+	  },
+	_move = function move( mappings ){
+		var map, sibs;
+		$.each(mappings, function(){
+			map = this;
+			map.parent 	= map.parent instanceof jQuery ? map.parent : $(map.parent);
+			map.child 	= map.child instanceof jQuery ? map.child : $(map.child);
+			sibs 		= map.parent.children();
+			if( !map.parent || !map.child )
+				return;
+			if( map.index === 0 ){
+				map.child.prependTo( map.parent );
+			}else if( typeof map.index == 'number' && map.index < sibs.length ){
+				sibs.eq(map.index).before(map.child);
+			}else{
+				map.child.appendTo( map.parent );
+			}
+		});
+	},
+	DocOrder = function(element, options){
+		this._defaults = defaults;
+		this._name = name;
+		this.$element = element;
+		this.settings = $.extend({}, defaults, options);
+		this.$win = $(window);
+		this.init();
+	};
+	
+	DocOrder.prototype.init = function(){
+		this._initState = (function(){
 			var state = [], parent, child;
-			$.each(settings.queries, function(){
+			$.each(this.settings.queries, function(){
 				$.each(this.map, function(){
 					child 	= $(this.child);
 					parent 	= child.parent();
 					if(child.length && parent.length == 1){
-						state.push({
+						state[state.length] = {
 							'parent': parent,
 							'child'	: child,
 							'index'	: child.index(),
-						});
+						};
 					}
 				});
 			});
 			return state;
-		})(),
-		
-		win = this,
-		
-		onWinResize = debounce(function(){
-			var w = win.width();
-			move( initState );
-			$.each(settings.queries, function(i){
-				var q = this;
-				/**
-				*	Check if the query applies to the current
-				*	window width
-				*/
-				if( (q.min == undefined || q.min <= w) &&
-					(q.max == undefined || q.max >= w) ){
-					move(q.map);				
-				}
-			});			
-		}, settings.latency);
-		
-		
-		win.resize(onWinResize);
-		
+		}).apply(this);
+		if( this.settings.autoUpdate ){
+			var api = this;
+			this.$win.resize(
+				_debounce(function(){
+					api.update();		
+				}, this.settings.latency)
+			);
+		}
+	};
+	
+	DocOrder.prototype.update = function(){
+		var w = this.$win.width();
+		_move( this._initState );
+		$.each( this.settings.queries, function(i){
+			var q = this;
+			/**
+			*	Check if the query applies to the current
+			*	window width
+			*/
+			if( (q.min == undefined || q.min <= w) &&
+				(q.max == undefined || q.max >= w) ){
+				_move(q.map);				
+			}
+		});
 		return this;
 	};
-})(jQuery);
+	
+	$.fn.docorder = function( options ){
+		
+		if( !$.data(this, 'docorder') ){
+			$.data(this, 'docorder', new DocOrder(this, options));
+		}
+		return this;
+	};
+	
+})(jQuery, window, document);
